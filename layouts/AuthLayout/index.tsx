e38e7bdbf04ad4a65e42dadaa04cd68c1f190d3d
Jerico Rillo <jericorillo@Jericos-MacBook-Air.local>
2023-11-28 02:01:47 +0800

    fix

--- layouts/AuthLayout/index.tsx
+++ layouts/AuthLayout/index.tsx
@@ -43,7 +43,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         <div className={styles.operatingHours}>
           <div className={styles.operatingHoursHeader}>Operating Hours</div>
           {operatingHours.map(op => 
-            <div className={styles.operatingHoursSchedule}>
+            <div key={op.day} className={styles.operatingHoursSchedule}>
               <div className={`${styles.operatingHoursDay} ${op.time === 'closed' ? styles.operatingHoursDayClosed : ''}`}>{op.day}</div>
               <div className={styles.operatingHoursTime}>{op.time}</div>
             </div>
