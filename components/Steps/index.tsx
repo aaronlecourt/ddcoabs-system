1dd32c8568b7c383360a163729fea0599f8cba3d
Jerico Rillo <jericorillo@Jericos-MacBook-Air.local>
2023-11-30 14:03:54 +0800

    [FEATURE] Step Component

--- components/Steps/index.tsx
+++ components/Steps/index.tsx
@@ -1,16 +1,53 @@
 import styles from './style.module.scss'
+import { Dispatch, SetStateAction } from 'react';
 
-export default function Steps({ children, ...props }: any) {
+interface Step {
+  label: string;
+  active: boolean;
+}
+
+interface StepsProps {
+  steps: Step[];
+  setSteps: Dispatch<SetStateAction<Step[]>>;
+  width?: number;
+}
+
+export default function Steps({ steps, setSteps, width }: StepsProps) {
+
+  const setActiveStep = (e: any, index: number) => {
+    e.preventDefault();
+
+    setSteps((prevSteps) => {
+      const updatedSteps = prevSteps.map((step: Step, i: number) => {
+        if (i <= index) {
+          return { ...step, active: true }
+        } else {
+          return { ...step, active: false }
+        }
+      })
+      return updatedSteps;
+    })
+  }
+
   return (
-    <div>
-      <div>
-        <div>01</div>
-        <div>Patient Form</div>
-      </div>
-      <div>
-        <div>02</div>
-        <div>Services</div>
-      </div>
-    </div>
+    <>
+      {steps && steps.length > 0 &&
+        <div className={styles.steps}>
+          {steps.map((step: { label: string, active: boolean }, index) =>
+            <div key={step.label} className={styles.steps__step}>
+              <div className={styles.step}>
+                <div className={`${styles.step__line} ${step.active ? styles.step__lineActive : ''}`}></div>
+                <div onClick={e => setActiveStep(e, index)}
+                  className={`${styles.step__number} ${step.active ? styles.step__numberActive : ''}`}>0{index + 1}</div>
+                <div className={`${styles.step__line} ${steps[index + 1]?.active ? styles.step__lineActive : ''}`}></div>
+              </div>
+              <div onClick={e => setActiveStep(e, index)}
+                className={styles.step__label}>{step.label}</div>
+            </div>
+          )}
+        </div>
+      }
+    </>
+
   )
 }
--- pages/book.tsx
+++ pages/book.tsx
@@ -2,17 +2,45 @@ import styles from '../styles/pages/home.module.scss'
 import PatientLayout from '../layouts/PatientLayout';
 import DentistLayout from '../layouts/DentistLayout';
 import useAuthGuard from '../guards/auth.guard';
+import Steps from '../components/Steps';
+import { useState } from 'react';
 
 
 export default function Book() {
   const { session, status } = useAuthGuard();
+  const [steps, setSteps] = useState([
+    {
+      label: 'Patient Form',
+      active: true
+    },
+    {
+      label: 'Services',
+      active: false
+    },
+    {
+      label: 'Date & Time',
+      active: false
+    },
+    {
+      label: 'Payment',
+      active: false
+    },
+    {
+      label: 'Confirmation',
+      active: false
+    }
+  ])
 
   const renderContent = () => {
     return (
       <>
         {session && (
           <main className={styles.main}>
-            <h1>DENTALFIX DENTAL CLINIC BOOKING PAGE!</h1>
+            <Steps 
+              steps={steps}
+              setSteps={setSteps}
+              width={500}
+            />
           </main>
         )}
       </>
