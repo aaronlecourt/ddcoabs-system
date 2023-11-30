4c83326682d1cfd9fe808ec6779b4bb33ec17ae4
Jerico Rillo <jericorillo@Jericos-MacBook-Air.local>
2023-11-30 11:48:01 +0800

    [FEATURE] Change Password Modal With Validation

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,10 +1,11 @@
 import styles from './style.module.scss'
 
-export default function Modal({ children, title, open, setOpen, ...props }: any) {
+export default function Modal({ children, title, open, setOpen, modalWidth, modalRadius, onClose, ...props }: any) {
   return (
     <div className={`${styles.modalContainer} ${open ? styles.modalContainerOpen : ''}`} {...props}>
-      <div className={styles.modal}>
-        <h1>{title}</h1>
+      <div className={styles.modalBackground} onClick={e => onClose ? onClose(e) : setOpen(false)}></div>
+      <div className={styles.modal} style={{ width: modalWidth || '80%', borderRadius: modalRadius + 'px' || 0 }}>
+        {title && <h1>{title}</h1>}
         {children}
       </div>
     </div>
---
+++ validations/changepassword.tsx
@@ -0,0 +1,53 @@
+import {
+  isPasswordValid
+} from '../utils/validation-rules';
+import { FormData, ErrorFormData } from '../types/changepassword';
+import { Dispatch, SetStateAction } from 'react';
+
+export const isChangePasswordFormValid = (formData: FormData, errorFormData: ErrorFormData, setErrorFormData: Dispatch<SetStateAction<ErrorFormData>>) => {
+  let result = true;
+
+  // Empty Fields
+  for (const field in formData) {
+    if (!formData[field as keyof FormData] && !errorFormData[field as keyof ErrorFormData].optional) {
+      setErrorFormData(prevErrorFormData => ({
+        ...prevErrorFormData,
+        [field]: {
+          optional: errorFormData[field as keyof ErrorFormData].optional || false,
+          error: true,
+          message: 'This field is required'
+        }
+      }))
+
+      result = false;
+    }
+  }
+
+  // Password Validation
+  if (formData.newPassword && !isPasswordValid(formData.newPassword)) {
+    setErrorFormData(prevErrorFormData => ({
+      ...prevErrorFormData,
+      ['newPassword']: {
+        error: true,
+        message: 'Password must contain at least 8 characters, 1 uppercase letter, and 1 special character'
+      }
+    }))
+
+    result = false;
+  }
+
+  // Confirm Password Validation
+  if (formData.confirmNewPassword && formData.confirmNewPassword && (formData.newPassword !== formData.confirmNewPassword)) {
+    setErrorFormData(prevErrorFormData => ({
+      ...prevErrorFormData,
+      ['confirmNewPassword']: {
+        error: true,
+        message: 'Password does not match'
+      }
+    }))
+
+    result = false;
+  }
+
+  return result;
+}
