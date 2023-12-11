ace3855602e8918d9e4c2fec14036d6f67e0f82b
Jerico Rillo <jericorillo@Jericos-MacBook-Air.local>
2023-12-11 23:41:13 +0800

    [FEATURE] Calendar Component and Style

--- forms/schedule.tsx
+++ forms/schedule.tsx
@@ -1,9 +1,14 @@
 import { forwardRef, useContext, useImperativeHandle } from "react";
 import styles from '../styles/forms/schedule.module.scss';
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCaretLeft, faCaretRight, faChevronLeft, faChevronRight, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../components/Button";
 import { BookingFormContext } from "../pages/book";
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
 
 const BookScheduleForm = forwardRef(({ }: any, ref) => {
   const { onStepNext, onStepBack,
@@ -27,12 +32,60 @@ const BookScheduleForm = forwardRef(({ }: any, ref) => {
     }
   }))
 
+  const navigateForward = () => {
+    const nextMonthDate = moment(selectedDate).add(1, 'months').toDate();
+    setSelectedDate(nextMonthDate);
+  };
+
+  const navigateBackward = () => {
+    const previousMonthDate = moment(selectedDate).subtract(1, 'months').toDate();
+    setSelectedDate(previousMonthDate);
+  };
+
+  const customToolbar = ({ label }: any) => {
+    return (
+      <div className="custom-calendar__toolbar">
+        <strong className="custom-calendar__toolbar__title">{label}</strong>
+        <div className="custom-calendar__toolbar__nav">
+          <FontAwesomeIcon onClick={navigateBackward}
+            icon={faChevronLeft} />
+          <FontAwesomeIcon onClick={navigateForward}
+            icon={faChevronRight} />
+        </div>
+      </div>
+    )
+  }
+
+  const formats = {
+    weekdayFormat: (date: any) => localizer.format(date, 'dd')[0],
+    dateFormat: (date: any) => localizer.format(date, 'D')
+  }
+
+  const handleSelectSlot = ({ start, end }: { start: Date; end: Date }) => {
+    setSelectedDate(start);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.form}>
         <div className={styles.form__container}>
           <strong>Select Date</strong>
-          <div className={styles.calendar}></div>
+          <div className={styles.calendarContainer}>
+          <Calendar
+            date={selectedDate}
+            onNavigate={() => null}
+            localizer={localizer}
+            formats={formats}
+            startAccessor="start"
+            endAccessor="end"
+            selectable
+            onSelectSlot={handleSelectSlot}
+            components={{
+              toolbar: customToolbar,
+            }}
+            className="custom-calendar"
+          />
+          </div>
         </div>
         <div className={styles.form__container}>
           <strong>Select Time</strong>
