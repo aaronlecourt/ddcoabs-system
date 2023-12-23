7961353834913725a89f49ad2050044c9befbd83
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-12-23 12:28:08 +0800

    patient appt chrt search, filter, sort, pagi DONE

--- pages/appointments.tsx
+++ pages/appointments.tsx
@@ -13,6 +13,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCancel,
   faChevronDown,
+  faChevronLeft,
+  faChevronRight,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/Button";
@@ -68,12 +70,17 @@ export default function Home({
   const [selectedAppointment, setSelectedAppointment] = useState<any>(null);
   const loggedInUserId = session?.user?.id;
 
-  const [selectedFilter, setSelectedFilter] = useState("Today"); // Set 'Today' as the default filter for the patient's section
-  const [filteredAppointments, setFilteredAppointments] = useState<
-    IAppointment[]
-  >(initialAppointmentData);
+  const [selectedFilter, setSelectedFilter] = useState('Today'); // Set 'Today' as the default filter for the patient's section
+  const [selectedSorting, setSelectedSorting] = useState('');
+  const [filteredAppointments, setFilteredAppointments] = useState<IAppointment[]>(initialAppointmentData);
   const [searchResults, setSearchResults] = useState<IAppointment[]>([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
+  const sortBy = ['Latest to Oldest', 'Oldest to Latest', 'Alphabetical (A-Z)', 'Alphabetical (Z-A)', 'Pending First'];
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const totalPages = Math.max(Math.ceil(filteredAppointments.length / itemsPerPage), 1);
+
   const onCancelAppointment = (appointment: any) => {
     setSelectedAppointment(appointment);
     setShowCancelAppointment(true);
@@ -107,167 +114,238 @@ export default function Home({
     }
   };
 
+  // const filterAppointmentsByStatus = (status: string) => {
+  //   const currentDate = new Date().toDateString(); // Get today's date
+
+  //   if (initialAppointmentData) {
+  //     const filteredAppointments = initialAppointmentData.filter(
+  //       (appointment: IAppointment) => {
+  //         const appointmentDate = new Date(appointment.date).toDateString();
+
+  //         if (status === "All") {
+  //           return true; // Show all appointments when 'All' is selected
+  //         } else if (status === "Today") {
+  //           return appointmentDate === currentDate; // Show appointments for today's date
+  //         }
+
+  //         return appointment.status === status; // Filter by other statuses
+  //       }
+  //     );
+
+  //     setFilteredAppointments(filteredAppointments); // Update the filteredAppointments state with the filtered list
+  //   }
+  // };
+
+  // // UseEffect to filter appointments for 'Today' when the component mounts
+  // useEffect(() => {
+  //   filterAppointmentsByStatus("Today");
+  //   handleSortChange("Latest to Oldest");
+  // }, []);
+
   const filterAppointmentsByStatus = (status: string) => {
     const currentDate = new Date().toDateString(); // Get today's date
-
+  
     if (initialAppointmentData) {
-      const filteredAppointments = initialAppointmentData.filter(
-        (appointment: IAppointment) => {
-          const appointmentDate = new Date(appointment.date).toDateString();
-
-          if (status === "All") {
-            return true; // Show all appointments when 'All' is selected
-          } else if (status === "Today") {
-            return appointmentDate === currentDate; // Show appointments for today's date
-          }
-
-          return appointment.status === status; // Filter by other statuses
+      const filteredAppointments = initialAppointmentData.filter((appointment: IAppointment) => {
+        const appointmentDate = new Date(appointment.date).toDateString();
+  
+        if (status === 'All') {
+          return true; // Show all appointments when 'All' is selected
+        } else if (status === 'Today') {
+          return appointmentDate === currentDate; // Show appointments for today's date
         }
-      );
-
+  
+        return appointment.status === status; // Filter by other statuses
+      });
+  
       setFilteredAppointments(filteredAppointments); // Update the filteredAppointments state with the filtered list
     }
   };
 
   // UseEffect to filter appointments for 'Today' when the component mounts
-  useEffect(() => {
-    filterAppointmentsByStatus("Today");
-  }, []);
+    useEffect(() => {
+      filterAppointmentsByStatus('Today');
+      handleSortChange('Latest to Oldest');
+    }, []);
 
-  const countAppointmentsByStatus = (status: string) => {
-    if (status === "All") {
-      return initialAppointmentData.length;
-    }
-    return initialAppointmentData.filter(
-      (appointment: IAppointment) => appointment.status === status
-    ).length;
-  };
+    const countAppointmentsByStatus = (status: string) => {
+      if (status === 'All') {
+        return initialAppointmentData.length;
+      }
+      return initialAppointmentData.filter((appointment:IAppointment) => appointment.status === status).length;
+    };
+    
+    const countAppointmentsForToday = () => {
+      const currentDate = new Date().toDateString();
+      return initialAppointmentData.filter((appointment: IAppointment) => {
+        const appointmentDate = new Date(appointment.date).toDateString();
+        return appointmentDate === currentDate;
+      }).length;
+    };
 
-  const countAppointmentsForToday = () => {
-    const currentDate = new Date().toDateString();
-    return initialAppointmentData.filter((appointment: IAppointment) => {
-      const appointmentDate = new Date(appointment.date).toDateString();
-      return appointmentDate === currentDate;
-    }).length;
-  };
-
-  const handleSearch = async (term: string) => {
-    setSearchTerm(term);
-
-    if (term.trim() !== "") {
-      const appointmentsToSearch =
-        filteredAppointments.length > 0
-          ? filteredAppointments
-          : initialAppointmentData;
-
-      const fetchedAppointments = await Promise.all(
-        appointmentsToSearch.map(async (appointment: IAppointment) => {
+    const handleSearch = async (term: string) => {
+      setSearchTerm(term);
+    
+      if (term.trim() !== '') {
+        const appointmentsToSearch = filteredAppointments.length > 0 ? filteredAppointments : initialAppointmentData;
+    
+        const fetchedAppointments = await Promise.all(appointmentsToSearch.map(async (appointment: IAppointment) => {
           if (appointment.patientId) {
-            const response = await fetch(
-              `/api/global/user/${appointment.patientId}`
-            );
+            const response = await fetch(`/api/global/user/${appointment.patientId}`);
             const patient = await response.json();
-
+    
             return {
               ...appointment,
-              patientName: patient ? patient.name || "" : "",
+              patientName: patient ? patient.name || '' : '',
             };
           }
           return appointment;
-        })
-      );
-
-      const filteredResults = fetchedAppointments.filter(
-        (appointment: IAppointment) => {
-          const matchesPatientName = appointment.patientName
-            ?.toLowerCase()
-            .includes(term.toLowerCase());
-          const matchesServiceName = appointment.dentistService
-            .toLowerCase()
-            .includes(term.toLowerCase());
-
+        }));
+    
+        const filteredResults = fetchedAppointments.filter((appointment: IAppointment) => {
+          const matchesPatientName = appointment.patientName?.toLowerCase().includes(term.toLowerCase());
+          const matchesServiceName = appointment.dentistService.toLowerCase().includes(term.toLowerCase());
+    
           return matchesPatientName || matchesServiceName;
-        }
-      );
-
-      setSearchResults(filteredResults as IAppointment[]);
-    } else {
-      setSearchResults([]); // Reset searchResults to an empty array when the search term is empty
-    }
-  };
-
-  const renderAppointmentsByStatus = (
-    appointmentsToMap: IAppointment[],
-    status: string,
-    searchTerm: string
-  ) => {
-    let filteredAppointments = appointmentsToMap;
-
-    if (status !== "All") {
-      filteredAppointments = appointmentsToMap.filter(
-        (appointment) => appointment.status === status
-      );
-    }
-
-    if (status === "Today") {
-      const currentDate = new Date().toDateString();
-      filteredAppointments = appointmentsToMap.filter(
-        (appointment: IAppointment) => {
+        });
+    
+        setSearchResults(filteredResults as IAppointment[]);
+      } else {
+        setSearchResults([]); // Reset searchResults to an empty array when the search term is empty
+      }
+      
+    };
+    
+    const renderAppointmentsByStatus = (appointmentsToMap: IAppointment[], status: string, searchTerm: string) => {
+      let filteredAppointments = appointmentsToMap;
+    
+      if (status !== 'All') {
+        filteredAppointments = appointmentsToMap.filter((appointment) => appointment.status === status);
+      }
+    
+      if (status === 'Today') {
+        const currentDate = new Date().toDateString();
+        filteredAppointments = appointmentsToMap.filter((appointment: IAppointment) => {
           const appointmentDate = new Date(appointment.date).toDateString();
           return appointmentDate === currentDate;
-        }
-      );
-    }
-
-    if (searchTerm.trim() !== "") {
-      const searchResults = filteredAppointments.filter(
-        (appointment: IAppointment) => {
-          const matchesPatientName = appointment.patientName
-            ?.toLowerCase()
-            .includes(searchTerm.toLowerCase());
-          const matchesServiceName = appointment.dentistService
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase());
-
-          return matchesPatientName || matchesServiceName;
-        }
-      );
-
-      if (searchResults.length > 0) {
-        filteredAppointments = searchResults;
-      } else {
-        filteredAppointments = []; // Empty array when no search results found within the filter
+        });
       }
-    }
+    
+      if (searchTerm.trim() !== '') {
+        const searchResults = filteredAppointments.filter((appointment: IAppointment) => {
+          const matchesPatientName = appointment.patientName?.toLowerCase().includes(searchTerm.toLowerCase());
+          const matchesServiceName = appointment.dentistService.toLowerCase().includes(searchTerm.toLowerCase());
+    
+          return matchesPatientName || matchesServiceName;
+        });
+    
+        if (searchResults.length > 0) {
+          filteredAppointments = searchResults;
+        } else {
+          filteredAppointments = [];
+        }
+      }
+    
+      // Pagination calculation
+      const totalCount = filteredAppointments.length; // Total count after filtering
+      const searchCount = searchResults.length; // Count of search results
+    
+      const totalCountForPagination = searchTerm.trim() !== '' ? searchCount : totalCount;
+      const totalPages = Math.max(Math.ceil(totalCountForPagination / itemsPerPage), 1);
+    
+      const validatedCurrentPage = Math.min(currentPage, totalPages);
+    
+      const indexOfLastItem = validatedCurrentPage * itemsPerPage;
+      const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+      const currentAppointments = filteredAppointments.slice(indexOfFirstItem, indexOfLastItem);
+    
+      return (
+        <>
+          {currentAppointments.length > 0 ? (
+            <>
+              {currentAppointments.map((appointment: IAppointment, index: number) => (
+                <Appointment key={index} appointment={appointment} onCancelAppointment={onCancelAppointment} isPatient={true} />
+              ))}
+              {renderPagination(totalPages, validatedCurrentPage)}
+            </>
+          ) : (
+            <div className={styles.appointments__empty}>No matching appointments found</div>
+          )}
 
-    return (
-      <>
-        {filteredAppointments.length > 0 ? (
-          filteredAppointments.map(
-            (appointment: IAppointment, index: number) => (
-              <Appointment
-                key={index}
-                appointment={appointment}
-                onCancelAppointment={onCancelAppointment}
-                isPatient={true}
-              />
-            )
-          )
-        ) : (
-          <div className={styles.appointments__empty}>
-            No matching appointments found
-          </div>
-        )}
-      </>
-    );
-  };
+        </>
+      );
+    };
+    
+    
+    
 
-  const handleFilterChange = (newFilter: string) => {
-    if (newFilter !== selectedFilter) {
-      setSelectedFilter(newFilter);
-      filterAppointmentsByStatus(newFilter);
-    }
-  };
+    const handleFilterChange = (newFilter: string) => {
+      if (newFilter !== selectedFilter) {
+        setSelectedFilter(newFilter);
+        filterAppointmentsByStatus(newFilter);
+      }
+
+      if (selectedSorting) {
+        handleSortChange(selectedSorting);
+      }
+    };
+    
+    const handleSortChange = (sortOption: string) => {
+      setSelectedSorting(sortOption);
+      let sortedAppointments = [...filteredAppointments]; // Create a copy of filteredAppointments
+    
+      switch (sortOption) {
+        case 'Oldest to Latest':
+          sortedAppointments.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+          break;
+        case 'Latest to Oldest':
+          sortedAppointments.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+          break;
+        case 'Alphabetical (A-Z)':
+          sortedAppointments.sort((a, b) => a.dentistService.localeCompare(b.dentistService));
+          break;
+        case 'Alphabetical (Z-A)':
+          sortedAppointments.sort((a, b) => b.dentistService.localeCompare(a.dentistService));
+          break;
+        case 'Pending First':
+          const pendingAppointments = sortedAppointments.filter(appointment => appointment.status === 'Pending');
+          const otherAppointments = sortedAppointments.filter(appointment => appointment.status !== 'Pending');
+    
+          pendingAppointments.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+          otherAppointments.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+    
+          sortedAppointments = [...pendingAppointments, ...otherAppointments];
+          break;
+        default:
+          break;
+      }
+    
+      setFilteredAppointments(sortedAppointments); // Update the state with the sorted array
+    };
+
+    const renderPagination = (totalPages: number, currentPage: number) => {
+      const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+    
+      return (
+        <div className={styles.pagination}>
+          <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+            <FontAwesomeIcon icon={faChevronLeft} width={16} height={16} color={'#737373'} />
+          </button>
+          {pageNumbers.map((number) => (
+            <button
+              key={number}
+              onClick={() => setCurrentPage(number)}
+              className={currentPage === number ? styles.active : ''}
+            >
+              {number}
+            </button>
+          ))}
+          <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>
+            <FontAwesomeIcon icon={faChevronRight} width={16} height={16} color={'#737373'} />
+          </button>
+        </div>
+      );
+    };
 
   const renderContent = () => {
     console.log("Appointments:", appointments);
@@ -334,6 +412,18 @@ export default function Home({
                   <div className={styles.filters__sort}>
                     <span className={styles.filters__sortTitle}>Sort By:</span>
                     <div className={styles.filters__sortDropdown}>
+                    <select onChange={(e) => handleSortChange(e.target.value)}>
+                      {sortBy.map((option, index) => (
+                        <option key={index} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                    </div>
+                  </div>
+                  {/* <div className={styles.filters__sort}>
+                    <span className={styles.filters__sortTitle}>Sort By:</span>
+                    <div className={styles.filters__sortDropdown}>
                       <span>Latest</span>
                       <FontAwesomeIcon
                         icon={faChevronDown}
@@ -342,7 +432,7 @@ export default function Home({
                         color={"#737373"}
                       />
                     </div>
-                  </div>
+                  </div> */}
                 </div>
                 <div className={styles.appointments}>
                   <div className={styles.appointments__filters}>
