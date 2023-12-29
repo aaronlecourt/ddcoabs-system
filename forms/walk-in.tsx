2287f4ca6fc338c1b8e3e8c2b1796d10dfcc358e
Aaron Lecourt <20217375@s.ubaguio.edu>
2023-12-29 17:27:17 +0800

    walk in FE done #1

--- forms/walk-in.tsx
+++ forms/walk-in.tsx
@@ -1,36 +1,44 @@
-import { Dispatch, SetStateAction, forwardRef, useContext, useEffect, useImperativeHandle, useState } from 'react'
-import { handleFormEnter, handleFormDataChange } from '../utils/form-handles'
-import styles from '../styles/forms/patient.module.scss'
-import { isPatientFormValid } from '../validations/patientform'
-import { BookingFormContextDentist } from '../pages/book'
-import Button from '../components/ArchiveButton'
+import {
+  Dispatch,
+  SetStateAction,
+  forwardRef,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
+import { handleFormEnter, handleFormDataChange } from "../utils/form-handles";
+import styles from "../styles/forms/patient.module.scss";
+import { isPatientFormValid } from "../validations/patientform";
+import { BookingFormContextDentist } from "../pages/book";
+import Button from "../components/Button";
 
 export const PatientWalkIn = {
-  patientName: '',
-  age: 0,
-  sex: '',
-  contactNumber: 0,
-  guardianName: '',
-  guardianNumber: 0,
-}
+  patientName: "",
+  age: "",
+  sex: "",
+  contactNumber: "",
+  guardianName: "",
+  guardianNumber: "",
+};
 
 export const ErrorPatientFormObjectDentist = {
-    firstName: { error: false, message: null },
-    lastName: { error: false, message: null },
-    age: { error: false, message: null },
-    sex: { error: false, message: null },
-    contactNumber: { error: false, message: null },
-    guardianName: { optional: true, error: false, message: null },
-    guardianNumber: { optional: true, error: false, message: null },
-}
+  firstName: { error: false, message: null },
+  lastName: { error: false, message: null },
+  age: { error: false, message: null },
+  sex: { error: false, message: null },
+  contactNumber: { error: false, message: null },
+  guardianName: { optional: true, error: false, message: null },
+  guardianNumber: { optional: true, error: false, message: null },
+};
 
-const BookPatientFormDentist = forwardRef(({ }: any, ref) => {
+const BookPatientFormDentist = forwardRef(({}: any, ref) => {
   const {
     patientFormDentist,
     setPatientFormDentist,
     patientErrorFormDataDentist,
     setPatientErrorFormDentist,
-    onStepNext
+    onStepNext,
   }: any = useContext(BookingFormContextDentist);
 
   const formData = patientFormDentist;
@@ -44,78 +52,150 @@ const BookPatientFormDentist = forwardRef(({ }: any, ref) => {
     setFormData((prevData: any) => ({
       ...prevData,
       [name]: value,
-      patientName: `${prevData.firstName} ${prevData.lastName}` 
-      
+      patientName: `${prevData.firstName} ${prevData.lastName}`,
     }));
-    console.log("PATIENT NAME: ", formData.patientName)
+    console.log("PATIENT NAME: ", formData.patientName);
   };
-  
-  useEffect(() => {
-    
-  }, [])
 
+  useEffect(() => {}, []);
 
   const next = (e: any) => {
     e.preventDefault();
-    console.log('Form Data:', formData);
+    console.log("Form Data:", formData);
     onStepNext(e);
-  }
+  };
 
   useImperativeHandle(ref, () => ({
     checkValidForm: () => {
-      return isPatientFormValid(formData, errorFormData, setErrorFormData)
-    }
-  }))
+      return isPatientFormValid(formData, errorFormData, setErrorFormData);
+    },
+  }));
 
   return (
-    <div className={styles.form}>
-      <div className={styles.form__container}>
-        <label> First Name: </label>
-        <input type = "text" name='firstName' placeholder="Jane or John" onChange={(e) => {handleFormDataChange(e, setFormData, setErrorFormData); updatePatientName(e);}} value={formData.firstName}/>
-      </div>
-      <div className={styles.form__container}>
-        <label> Last Name: </label>
-        <input type = "text" name='lastName' placeholder='Doe' onChange={(e) => {handleFormDataChange(e, setFormData, setErrorFormData); updatePatientName(e);}} value={formData.lastName} />
-      </div>
-
-      <div className={styles.form__container}>
-        <label> Age: </label>
-        <input type = "number" name='age' onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)} value={formData.age}/>
-      </div>
-
-      <div className={styles.form__container}>
-        <span> Sex: </span>
-
-        <input type = "radio" id='female' name='sex' value="Female" onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)}/>
-        <label htmlFor='female'> Female </label>
+    <div className={styles.formContain}>
+      <div className={styles.form}>
+        <div className={styles.form__row}>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> First Name: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="Jane or John"
+              onChange={(e) => {
+                handleFormDataChange(e, setFormData, setErrorFormData);
+                updatePatientName(e);
+              }}
+              value={formData.firstName}
+            />
+          </div>
+        </div>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Last Name: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Doe"
+              onChange={(e) => {
+                handleFormDataChange(e, setFormData, setErrorFormData);
+                updatePatientName(e);
+              }}
+              value={formData.lastName}
+            />
+          </div>
+        </div>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Age: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="number"
+              name="age"
+              onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+              value={formData.age}
+            />
+          </div>
+        </div>
+        </div>
         
-        <input type = "radio" id='male' name='sex' value="Male" onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)}/>
-        <label htmlFor='male'> Male </label>
-        
+        <div className={styles.form__row}>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Sex: </label>
+          </div>
+          <input
+            type="radio"
+            id="female"
+            name="sex"
+            value="Female"
+            onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+          />
+          <label htmlFor="female"> Female </label>
+          <input
+            type="radio"
+            id="male"
+            name="sex"
+            value="Male"
+            onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+          />
+          <label htmlFor="male"> Male </label>
+        </div>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Contact Number: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="text"
+              placeholder="09123456789"
+              name="contactNumber"
+              onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+              value={formData.contactNumber}
+            />
+          </div>
+        </div>
+        </div>
+        <hr />
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Parent's or Guardians Name: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="text"
+              placeholder="N/A"
+              onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+              name="guardianName"
+              value={formData.guardianName}
+            />
+          </div>
+        </div>
+        <div className={styles.form__row__field}>
+          <div className={styles.form__row__field__label}>
+            <label> Guardian Contact Number: </label>
+          </div>
+          <div className={styles.form__Input}>
+            <input
+              type="text"
+              placeholder="09123456789"
+              name="guardianNumber"
+              onChange={(e) => handleFormDataChange(e, setFormData, setErrorFormData)}
+              value={formData.guardianNumber}
+            />
+          </div>
+        </div>
       </div>
-
-      <div className={styles.form__container}>
-        <label> Contact Number: </label>
-        <input type = "number" name='contactNumber' onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)} value={formData.contactNumber}/>
-      </div>
-
-      <hr></hr>
-
-      <div className={styles.form__container}>
-        <label> Parent's or Guardians Name: </label>
-        <input type = "text" placeholder='N/A' onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)} name='guardianName' value={formData.guardianName}/>
-      </div>
-
-      <div className={styles.form__container}>
-        <label> Contact Number: </label>
-        <input type = "number" name='guardianNumber' onChange={e => handleFormDataChange(e, setFormData, setErrorFormData)} value={formData.guardianNumber}/>
-      </div>
-
       <div className={styles.next}>
         <Button onClick={next}>Next</Button>
       </div>
-      
     </div>
-  )
-})
+  );
+  
+});
 export default BookPatientFormDentist;
