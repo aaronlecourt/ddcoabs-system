53a7ca0d180b68856fc92a53f8c51a3e20c34f77
Sarah Dominguez Perea <sdperea1@up.edu.ph>
2024-01-01 22:49:20 +0800

    Appointment filtering BE

--- pages/api/global/appointment/index.ts
+++ pages/api/global/appointment/index.ts
@@ -1,6 +1,7 @@
 import connectMongo from "../../../../utils/connectMongo";
 import Appointment from "../../../../models/Appointment";
 import type { NextApiRequest, NextApiResponse } from "next";
+import APPOINTMENT_STATUS from "../../../../constants/appointmentStatus";
 
 export default async function appointmentHandler(
   req: NextApiRequest,
@@ -14,68 +15,77 @@ export default async function appointmentHandler(
     switch (method) {
       case "GET":
         try {
-          const { status = "All", search, sortBy } = query;
+          let appointments = [];
 
-          let appointmentQuery = {};
+          if (query) {
+            let appointmentQuery = {};
+            const { status = "All", search, sortBy } = query;
 
-          // Prepare the query based on status and search parameters
-          if (status === "Today") {
-            const today = new Date();
-            appointmentQuery = {
-              date: {
-                $gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()),
-                $lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1),
-              },
-            };
-          } else if (status !== "All") {
-            appointmentQuery = { status };
-          }
+            Object.keys(query).map(v => {
+              if (v == 'status' && status) {
+                // Prepare the query based on status and search parameters
+                if (status === "Today") {
+                  const today = new Date();
+                  Object.assign(appointmentQuery, {
+                    date: {
+                      $gte: new Date(today.getFullYear(), today.getMonth(), today.getDate()),
+                      $lt: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1),
+                    },
+                  })
+                } else if (status !== "All") {
+                  Object.assign(appointmentQuery, { status });
+                }
+              } else if (v == 'search' && search) {
+                const searchQuery = {
+                  $or: [
+                    { patientName: { $regex: search, $options: "i" } },
+                    { dentistService: { $regex: search, $options: "i" } },
+                    // Add more fields here based on your Appointment model
+                  ],
+                };
 
-          if (search) {
-            const searchQuery = {
-              $or: [
-                { patientName: { $regex: search, $options: "i" } },
-                { dentistService: { $regex: search, $options: "i" } },
-                // Add more fields here based on your Appointment model
-              ],
-            };
+                appointmentQuery = status === "All"
+                  ? { ...searchQuery }
+                  : { ...appointmentQuery, ...searchQuery };
+              } else if (v == 'dentistService') {
+                Object.assign(appointmentQuery, { [v]: { '$regex': `${query[v]}`, '$options': 'i' } })
+              } else if (v != 'sortBy') {
+                Object.assign(appointmentQuery, { [v]: query[v] })
+              }
+            })
 
-            appointmentQuery = status === "All"
-              ? { ...searchQuery }
-              : { ...appointmentQuery, ...searchQuery };
-          }
+            appointments = await Appointment.find(appointmentQuery);
 
-          let appointments = await Appointment.find(appointmentQuery);
-
-          // Apply sorting based on sortBy parameter
-          if (sortBy) {
-            switch (sortBy) {
-              case "Oldest to Latest":
-                appointments = appointments.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-                break;
-              case "Latest to Oldest":
-                appointments = appointments.sort((a, b) => new Date(b.date).getTime()- new Date(a.date).getTime());
-                break;
-              case "Alphabetical (A-Z)":
-                appointments = appointments.sort((a, b) => a.dentistService.localeCompare(b.dentistService));
-                break;
-              case "Alphabetical (Z-A)":
-                appointments = appointments.sort((a, b) => b.dentistService.localeCompare(a.dentistService));
-                break;
-              case "Pending First":
-                appointments = appointments.sort((a, b) => {
-                  if (a.status === "Pending" && b.status !== "Pending") {
-                    return -1; // "Pending" appointments come first
-                  } else if (a.status !== "Pending" && b.status === "Pending") {
-                    return 1; // "Pending" appointments come after other statuses
-                  } else {
-                    return 0; // Maintain the order for other statuses
-                  }
-                });
-                break;
-              // Add more cases for additional sorting criteria if needed
-              default:
-                break;
+            // Apply sorting based on sortBy parameter
+            if (sortBy) {
+              switch (sortBy) {
+                case "Oldest to Latest":
+                  appointments = appointments.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
+                  break;
+                case "Latest to Oldest":
+                  appointments = appointments.sort((a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime());
+                  break;
+                case "Alphabetical (A-Z)":
+                  appointments = appointments.sort((a: any, b: any) => a.dentistService.localeCompare(b.dentistService));
+                  break;
+                case "Alphabetical (Z-A)":
+                  appointments = appointments.sort((a: any, b: any) => b.dentistService.localeCompare(a.dentistService));
+                  break;
+                case "Pending First":
+                  appointments = appointments.sort((a: any, b: any) => {
+                    if (a.status === "Pending" && b.status !== "Pending") {
+                      return -1; // "Pending" appointments come first
+                    } else if (a.status !== "Pending" && b.status === "Pending") {
+                      return 1; // "Pending" appointments come after other statuses
+                    } else {
+                      return 0; // Maintain the order for other statuses
+                    }
+                  });
+                  break;
+                // Add more cases for additional sorting criteria if needed
+                default:
+                  break;
+              }
             }
           }
 
